3fd36a4ecf2ab6eeed06bc41211385cbd95accc0
User <https://github.com/Vla-a/Mr-jd2-78-21.git>
2021-10-18 09:35:16 +0300

    finish

--- app/src/main/java/com/example/currency/restApi/CurrencyRepository.kt
+++ app/src/main/java/com/example/currency/restApi/CurrencyRepository.kt
@@ -23,11 +23,12 @@ class CurrencyRepository(
     @RequiresApi(Build.VERSION_CODES.O)
     val tommorow = LocalDate.now().plus(1, ChronoUnit.DAYS)
         .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
     @RequiresApi(Build.VERSION_CODES.O)
     val yeasDay = LocalDate.now().plus(-1, ChronoUnit.DAYS)
         .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
     val toDay = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).format(System.currentTimeMillis())
-
+    var date = ""
 
 
     suspend fun addCurrency(currencyList: MutableList<CurrencyResult>) {
@@ -40,7 +41,7 @@ class CurrencyRepository(
                 it.name,
                 it.rate,
                 it.nam,
-               it.rateNewDay
+                it.rateNewDay
             )
         })
 
@@ -102,42 +103,47 @@ class CurrencyRepository(
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getCurrenciesResult(): MutableList<CurrencyResult> {
 
-         if(currencyApi.getCharacterListDay(yeasDay) != null) {
-             return   withContext(Dispatchers.IO) {
+        if (currencyApi.getCharacterListDay(tommorow) == null) {
 
-            currencyApi.getCharacterListDay(yeasDay).map {
+            return withContext(Dispatchers.IO) {
 
-                CurrencyResult(
-                    it.id,
-                    it.numCod,
-                    it.charCode,
-                    it.scale,
-                    it.name,
-                    it.rate,
-                    nam = ist(it.charCode),
-                    mist( it.charCode)
+                currencyApi.getCharacterListDay(tommorow).map {
 
-                )}
-        } as MutableList<CurrencyResult>
-        } else{
-             return   withContext(Dispatchers.IO) {
+                    CurrencyResult(
+                        it.id,
+                        it.numCod,
+                        it.charCode,
+                        it.scale,
+                        it.name,
+                        it.rate,
+                        nam = ist(it.charCode),
+                        mist(it.charCode)
 
-                 currencyApi.getCharacterListDay(tommorow).map {
-
-                     CurrencyResult(
-                         it.id,
-                         it.numCod,
-                         it.charCode,
-                         it.scale,
-                         it.name,
-                         it.rate,
-                         nam = ist(it.charCode),
-                         mist( it.charCode)
-
-                     )}
-             } as MutableList<CurrencyResult>
+                    )
+                }
+            } as MutableList<CurrencyResult>
         }
+
+            return withContext(Dispatchers.IO) {
+
+                currencyApi.getCharacterListDay(yeasDay).map {
+
+                    CurrencyResult(
+                        it.id,
+                        it.numCod,
+                        it.charCode,
+                        it.scale,
+                        it.name,
+                        it.rate,
+                        nam = ist(it.charCode),
+                        mist(it.charCode)
+
+                    )
+                }
+            } as MutableList<CurrencyResult>
+
     }
+
     private fun ist(charCod: String): String {
 
         if (charCod == "EUR" || charCod == "RUB" || charCod == "USD") return "true"
@@ -148,34 +154,88 @@ class CurrencyRepository(
     private suspend fun mist(charCof: String): Double {
 
         when (charCof) {
-            "AUD" -> { return   getCurrenciesListDay()[0].rate }
-            "AMD" -> { return   getCurrenciesListDay()[1].rate }
-            "BGN" -> { return   getCurrenciesListDay()[2].rate }
-            "UAH" -> { return   getCurrenciesListDay()[3].rate }
-            "DKK" -> { return   getCurrenciesListDay()[4].rate }
-            "USD" -> { return   getCurrenciesListDay()[5].rate }
-            "EUR" -> { return   getCurrenciesListDay()[6].rate }
-            "PLN" -> { return   getCurrenciesListDay()[7].rate }
-            "JPY" -> { return   getCurrenciesListDay()[8].rate }
-            "IRR" -> { return   getCurrenciesListDay()[9].rate }
-            "ISK" -> { return   getCurrenciesListDay()[10].rate }
-            "CAD" -> { return   getCurrenciesListDay()[11].rate }
-            "CNY" -> { return   getCurrenciesListDay()[12].rate }
-            "KWD" -> { return   getCurrenciesListDay()[13].rate }
-            "MDL" -> { return   getCurrenciesListDay()[14].rate }
-            "NZD" -> { return   getCurrenciesListDay()[15].rate }
-            "NOK" -> { return   getCurrenciesListDay()[16].rate }
-            "RUB" -> { return   getCurrenciesListDay()[17].rate }
-            "XDR" -> { return   getCurrenciesListDay()[18].rate }
-            "SGD" -> { return   getCurrenciesListDay()[19].rate }
-            "KGS" -> { return   getCurrenciesListDay()[20].rate }
-            "KZT" -> { return   getCurrenciesListDay()[21].rate }
-            "TRY" -> { return   getCurrenciesListDay()[22].rate }
-            "GBP" -> { return   getCurrenciesListDay()[23].rate }
-            "CZK" -> { return   getCurrenciesListDay()[24].rate }
-            "SEK" -> { return   getCurrenciesListDay()[25].rate }
-            "CHF" -> { return   getCurrenciesListDay()[26].rate }
+            "AUD" -> {
+                return getCurrenciesListDay()[0].rate
+            }
+            "AMD" -> {
+                return getCurrenciesListDay()[1].rate
+            }
+            "BGN" -> {
+                return getCurrenciesListDay()[2].rate
+            }
+            "UAH" -> {
+                return getCurrenciesListDay()[3].rate
+            }
+            "DKK" -> {
+                return getCurrenciesListDay()[4].rate
+            }
+            "USD" -> {
+                return getCurrenciesListDay()[5].rate
+            }
+            "EUR" -> {
+                return getCurrenciesListDay()[6].rate
+            }
+            "PLN" -> {
+                return getCurrenciesListDay()[7].rate
+            }
+            "JPY" -> {
+                return getCurrenciesListDay()[8].rate
+            }
+            "IRR" -> {
+                return getCurrenciesListDay()[9].rate
+            }
+            "ISK" -> {
+                return getCurrenciesListDay()[10].rate
+            }
+            "CAD" -> {
+                return getCurrenciesListDay()[11].rate
+            }
+            "CNY" -> {
+                return getCurrenciesListDay()[12].rate
+            }
+            "KWD" -> {
+                return getCurrenciesListDay()[13].rate
+            }
+            "MDL" -> {
+                return getCurrenciesListDay()[14].rate
+            }
+            "NZD" -> {
+                return getCurrenciesListDay()[15].rate
+            }
+            "NOK" -> {
+                return getCurrenciesListDay()[16].rate
+            }
+            "RUB" -> {
+                return getCurrenciesListDay()[17].rate
+            }
+            "XDR" -> {
+                return getCurrenciesListDay()[18].rate
+            }
+            "SGD" -> {
+                return getCurrenciesListDay()[19].rate
+            }
+            "KGS" -> {
+                return getCurrenciesListDay()[20].rate
+            }
+            "KZT" -> {
+                return getCurrenciesListDay()[21].rate
+            }
+            "TRY" -> {
+                return getCurrenciesListDay()[22].rate
+            }
+            "GBP" -> {
+                return getCurrenciesListDay()[23].rate
+            }
+            "CZK" -> {
+                return getCurrenciesListDay()[24].rate
+            }
+            "SEK" -> {
+                return getCurrenciesListDay()[25].rate
+            }
+            "CHF" -> {
+                return getCurrenciesListDay()[26].rate
+            }
         }
-        return  getCurrenciesListDay()[0].rate
+        return getCurrenciesListDay()[0].rate
     }
 }
