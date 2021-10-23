1e21b311c26612b6f1a2c34782e94dd62152c3c6
User <https://github.com/Vla-a/Mr-jd2-78-21.git>
2021-10-23 17:01:07 +0300

    finish

--- app/src/main/java/com/example/currency/restApi/CurrencyRepository.kt
+++ app/src/main/java/com/example/currency/restApi/CurrencyRepository.kt
@@ -20,6 +20,7 @@ class CurrencyRepository(
     private val currencyApi: CurrencyApi
 ) {
 
+    var list = mutableListOf<Currency>()
 
     @RequiresApi(Build.VERSION_CODES.O)
     val tommorow = LocalDate.now().plus(1, ChronoUnit.DAYS)
@@ -71,7 +72,7 @@ class CurrencyRepository(
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getCurrenciesResult(): MutableList<CurrencyResult> {
         val listTommorow = currencyApi.getCharacterListDay(tommorow)
-        val list = currencyApi.getCharacterListDay(toDay).map {
+        list = currencyApi.getCharacterListDay(toDay).map {
             Currency(
                 it.id,
                 it.numCod,
@@ -98,7 +99,7 @@ class CurrencyRepository(
                         it.name,
                         it.rate,
                         nam = ist(it.charCode),
-                        mist(list)
+                        mist(it.charCode)
 
                     )
                 }
@@ -116,7 +117,7 @@ class CurrencyRepository(
                         it.name,
                         it.rate,
                         nam = ist(it.charCode),
-                        mist(list)
+                        mist(it.charCode)
                     )
                 }
             } as MutableList<CurrencyResult>
@@ -132,93 +133,93 @@ class CurrencyRepository(
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private suspend fun mist(charCof: MutableList<Currency>): Double {
+    private suspend fun mist(charCof: String): Double {
 
-        charCof.forEach {
-            when (it.charCode) {
-                "AUD" -> {
-                    return charCof[0].rate
-                }
-                "AMD" -> {
-                    return charCof[1].rate
-                }
-                "BGN" -> {
-                    return charCof[2].rate
-                }
-                "UAH" -> {
-                    return charCof[3].rate
-                }
-                "DKK" -> {
-                    return charCof[4].rate
-                }
-                "USD" -> {
-                    return charCof[5].rate
-                }
-                "EUR" -> {
-                    return charCof[6].rate
-                }
-                "PLN" -> {
-                    return charCof[7].rate
-                }
-                "JPY" -> {
-                    return charCof[8].rate
-                }
-                "IRR" -> {
-                    return charCof[9].rate
-                }
-                "ISK" -> {
-                    return charCof[10].rate
-                }
-                "CAD" -> {
-                    return charCof[11].rate
-                }
-                "CNY" -> {
-                    return charCof[12].rate
-                }
-                "KWD" -> {
-                    return charCof[13].rate
-                }
-                "MDL" -> {
-                    return charCof[14].rate
-                }
-                "NZD" -> {
-                    return charCof[15].rate
-                }
-                "NOK" -> {
-                    return charCof[16].rate
-                }
-                "RUB" -> {
-                    return charCof[17].rate
-                }
-                "XDR" -> {
-                    return charCof[18].rate
-                }
-                "SGD" -> {
-                    return charCof[19].rate
-                }
-                "KGS" -> {
-                    return charCof[20].rate
-                }
-                "KZT" -> {
-                    return charCof[21].rate
-                }
-                "TRY" -> {
-                    return charCof[22].rate
-                }
-                "GBP" -> {
-                    return charCof[23].rate
-                }
-                "CZK" -> {
-                    return charCof[24].rate
-                }
-                "SEK" -> {
-                    return charCof[25].rate
-                }
-                "CHF" -> {
-                    return charCof[26].rate
-                }
+
+        when (charCof) {
+            "AUD" -> {
+                return list[0].rate
             }
+            "AMD" -> {
+                return list[1].rate
+            }
+            "BGN" -> {
+                return list[2].rate
+            }
+            "UAH" -> {
+                return list[3].rate
+            }
+            "DKK" -> {
+                return list[4].rate
+            }
+            "USD" -> {
+                return list[5].rate
+            }
+            "EUR" -> {
+                return list[6].rate
+            }
+            "PLN" -> {
+                return list[7].rate
+            }
+            "JPY" -> {
+                return list[8].rate
+            }
+            "IRR" -> {
+                return list[9].rate
+            }
+            "ISK" -> {
+                return list[10].rate
+            }
+            "CAD" -> {
+                return list[11].rate
+            }
+            "CNY" -> {
+                return list[12].rate
+            }
+            "KWD" -> {
+                return list[13].rate
+            }
+            "MDL" -> {
+                return list[14].rate
+            }
+            "NZD" -> {
+                return list[15].rate
+            }
+            "NOK" -> {
+                return list[16].rate
+            }
+            "RUB" -> {
+                return list[17].rate
+            }
+            "XDR" -> {
+                return list[18].rate
+            }
+            "SGD" -> {
+                return list[19].rate
+            }
+            "KGS" -> {
+                return list[20].rate
+            }
+            "KZT" -> {
+                return list[21].rate
+            }
+            "TRY" -> {
+                return list[22].rate
+            }
+            "GBP" -> {
+                return list[23].rate
+            }
+            "CZK" -> {
+                return list[24].rate
+            }
+            "SEK" -> {
+                return list[25].rate
+            }
+            "CHF" -> {
+                return list[26].rate
+            }
+
         }
-        return getCurrenciesListDay()[0].rate
+        return list[26].rate
     }
 }
